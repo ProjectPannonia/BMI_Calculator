3ecb8c025ed74640db7cb98a302463caccc19620
Adam Letenyei <letenyeidam89@gmail.com>
2020-02-06 22:13:41 +0100

    isValidArguments and giveTheResult function implemented

--- src/main/java/com/bmi/BmiCalculator.java
+++ src/main/java/com/bmi/BmiCalculator.java
@@ -4,16 +4,22 @@ public class BmiCalculator {
     private double height;
     private double weight;
 
-    public BmiCalculator(double height,double weight){
+    public BmiCalculator(double height, double weight) {
         this.height = height;
         this.weight = weight;
     }
 
-    protected String calculate(){
+    protected String calculate() {
+        String result = giveTheResult(height, weight);
+        return result;
+    }
+
+    private String giveTheResult(double height, double weight) {
         String result;
-        if (!isValidArguments(height,weight)){
+        boolean isNotZero = isValidArguments(height, weight);
+        if (!isNotZero) {
             result = "Hiba a bevitt adatokban!\n Az adat nem lehet nulla.";
-        }else {
+        } else {
             double weightPerThousand = height / 100;
             double bmi = weight / (weightPerThousand * weightPerThousand);
             String bmiText = String.valueOf(bmi).substring(0, 4);
@@ -21,28 +27,30 @@ public class BmiCalculator {
         }
         return result;
     }
-    private boolean isValidArguments(double height,double weight){
+
+    private boolean isValidArguments(double height, double weight) {
         //Return true - not null arguments
         boolean isNotZero = height > 0 && weight > 0;
         return isNotZero;
     }
-    private String result(double bmi){
+
+    private String result(double bmi) {
         String result;
-        if(bmi < 16.0){
+        if (bmi < 16.0) {
             result = "Súlyosan sovány.";
-        }else if(bmi >= 16.0 && bmi <= 17.0){
+        } else if (bmi >= 16.0 && bmi <= 17.0) {
             result = "Közepesen sovány.";
-        }else if(bmi > 17.0 && bmi <= 18.5){
+        } else if (bmi > 17.0 && bmi <= 18.5) {
             result = "Enyhén sovány.";
-        }else if(bmi > 18.5 && bmi <= 25.0){
+        } else if (bmi > 18.5 && bmi <= 25.0) {
             result = "Egészséges";
-        }else if(bmi > 25.0 && bmi <= 30){
+        } else if (bmi > 25.0 && bmi <= 30) {
             result = "Túlsúlyos.";
-        }else if(bmi > 30 && bmi <= 35.0){
+        } else if (bmi > 30 && bmi <= 35.0) {
             result = "Enyhén elhízott.";
-        }else if(bmi > 35.0 && bmi <= 40.0){
+        } else if (bmi > 35.0 && bmi <= 40.0) {
             result = "Közepesen elhízott.";
-        }else{
+        } else {
             result = "Súlyosan elhízott.";
         }
         return result;
--- src/main/java/com/bmi/Main.java
+++ src/main/java/com/bmi/Main.java
@@ -2,7 +2,7 @@ package com.bmi;
 
 public class Main {
     public static void main(String[] args) {
-        BmiCalculator bmi = new BmiCalculator(10,0);
+        BmiCalculator bmi = new BmiCalculator(10,10);
         System.out.println(bmi.calculate());
     }
 }
