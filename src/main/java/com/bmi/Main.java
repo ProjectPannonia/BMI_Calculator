0a9fb75126b6846a9f6e30819305275c05a4e304
Adam Letenyei <letenyeidam89@gmail.com>
2020-02-06 21:23:14 +0100

    Two more test cases

--- src/main/java/com/bmi/Main.java
+++ src/main/java/com/bmi/Main.java
@@ -2,7 +2,7 @@ package com.bmi;
 
 public class Main {
     public static void main(String[] args) {
-        BmiCalculator bmi = new BmiCalculator(180,90);
+        BmiCalculator bmi = new BmiCalculator(200,105);
         System.out.println(bmi.calculate());
     }
 }
--- src/test/java/com/bmi/testBmiCalculator.java
+++ src/test/java/com/bmi/testBmiCalculator.java
@@ -6,21 +6,42 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class testBmiCalculator {
-    BmiCalculator calculator = null;
+    BmiCalculator firstCalculator = null;
+    BmiCalculator secondCalculator = null;
+    BmiCalculator thirdCalculator = null;
 
     @Before
     public void init(){
-        calculator = new BmiCalculator(180,90);
+        firstCalculator = new BmiCalculator(180,90);
+        secondCalculator = new BmiCalculator(165,75);
+        thirdCalculator = new BmiCalculator(200,105);
     }
     @Test
-    public void testCalculator(){
+    public void testFirstCalculator(){
+        //Height: 180,Weight: 90
         String expectedBmi = "Túlsúlyos.";
         String expectedString = "Your BMI is: " + 27.7 + "\n" + expectedBmi;
-        String result = calculator.calculate();
+        String result = firstCalculator.calculate();
+        assertEquals(expectedString,result);
+    }
+    @Test
+    public void testSecondCalculator(){
+        //Height: 165,Weight: 75
+        String expectedBmi = "Túlsúlyos.";
+        String expectedString = "Your BMI is: " + 27.5 + "\n" + expectedBmi;
+        String result = secondCalculator.calculate();
+        assertEquals(expectedString,result);
+    }
+    @Test
+    public void testThirdCalculator(){
+        //Height: 200,Weight:105
+        String expectedBmi = "Túlsúlyos.";
+        String expectedString = "Your BMI is: " + 26.2 + "\n" + expectedBmi;
+        String result = thirdCalculator.calculate();
         assertEquals(expectedString,result);
     }
     @After
     public void setToNull(){
-        calculator = null;
+        firstCalculator = null;
     }
 }
